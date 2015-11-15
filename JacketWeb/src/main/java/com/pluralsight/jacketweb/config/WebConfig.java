df1e74fa4f0b89b8b96025e3ffbf5eab9c25408c
Kevin Jones <kevin@rocksolidknowledge.com>
2015-11-15 11:03:50 +0000

    Remove unused imports

--- JacketWeb/src/main/java/com/pluralsight/jacketweb/config/WebConfig.java
+++ JacketWeb/src/main/java/com/pluralsight/jacketweb/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.pluralsight.jacketweb.config;
 
-import java.util.Locale;
 import java.util.Properties;
 
 import javax.inject.Inject;
@@ -30,15 +29,10 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
